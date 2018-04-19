bd6d0604f7950e41577b2f70d68188c1640717c9
Michael Zipp <mzipp3@mail.gatech.edu>
2018-04-19 17:27:28 -0400

    Update with basic window

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-require("express");
-require("electron");
+var express = require("express");
+var {app, BrowserWindow} = require("electron");
+var hue = require("node-hue-api");
+var url = require("url");
+var path = require("path");
+
+//Basic window creation
+function createWindow(){
+  win = new BrowserWindow({width:800,height: 600,show: false})
+  global.win = win;
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'views/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('ready-to-show',() => {
+    win.show();
+  })
+
+  win.on('closed',() => {
+    win=null;
+  })
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  console.log('Closing application');
+  app.quit();
+});
