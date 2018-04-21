6d006c5be99904943c682bd12c0d34e27c3c254b
Michael Zipp <mzipp3@mail.gatech.edu>
2018-04-21 19:11:07 -0400

    Finish messing w/ canvas color wheel

--- render.js
+++ render.js
@@ -11,15 +11,17 @@ function drawCanvasCircle(){
       sy = CY-10;
 
   graphics.beginPath();
-  graphics.arc(CX,CY,sx+5,0,2*Math.PI);
+  graphics.arc(CX,CY,sx+2,0,2*Math.PI);
   graphics.fill();
   for (var i = 0; i < 360; i += 0.1) {
       var rad = i * (2 * Math.PI) / 360;
       var grad = graphics.createLinearGradient(CX, CY, CX + sx * Math.cos(rad), CY + sy * Math.sin(rad));
-      grad.addColorStop(0, "white");
-      grad.addColorStop(0.05, "white");
+      grad.addColorStop(0, "hsla(" + i + ", 0%, 100%, 1.0)");
+      grad.addColorStop(0.25, "hsla(" + i + ", 100%, 87.5%, 1.0)");
+      grad.addColorStop(0.5, "hsla(" + i + ", 100%, 75%, 1.0)");
+      // grad.addColorStop(0.93, "hsla(" + i + ", 100%, 50%, 1.0)");
       grad.addColorStop(0.95, "hsla(" + i + ", 100%, 50%, 1.0)");
-      grad.addColorStop(1, "hsla(" + i + ", 100%, 50%, 1.0)");
+      grad.addColorStop(1, "hsla(" + i + ", 100%, 0%, 1.0)");
       graphics.strokeStyle = grad;
       graphics.beginPath();
       graphics.moveTo(CX, CY);
@@ -54,3 +56,18 @@ if (hue1 >= 0 && hue1 <= 1) {
     // Change r,g,b values from [0,1] to [0,255]
     return [255*r,255*g,255*b];
 }
+function rgb2Hsv(r,g,b){
+
+}
+function rgb2hex(r,g,b){
+  var rHex = r.toString(16);
+  var bHex = b.toString(16);
+  var gHex = g.toString(16);
+  //Add leading zeroes if needed
+  if(r < 16 ) rHex = "0" + rHex;
+  if(g < 16 ) gHex = "0" + gHex;
+  if(b < 16 ) bHex = "0" + bHex;
+  return "#"+rHex+gHex+bHex;
+}
+
+function getColor(){}
